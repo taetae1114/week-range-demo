5fdb256119d314e587cfead5186d950fbf4b7aeb
taetae1114 <phylosa@163.com>
2022-10-11 11:57:42 +0800

    修改部署到github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path"
+
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+  
+
   plugins: [
     vue()
+    
+
   ],
   base:'/weekrange/'
 })
